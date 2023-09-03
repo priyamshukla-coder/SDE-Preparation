6f908ec54550baeea35e3055557384b3003d6564
priyamshukla-coder <priyamshukla66702@gmail.com>
2023-09-03 12:47:15 +0530

    Added Check if Strings can be made equal with Operations ii

---
+++ Check if Strings can be made equal with Operations II.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        
+        if s1==s2:
+            
+            return True
+        
+        e1,o1,e2,o2=Counter(),Counter(),Counter(),Counter()
+        
+        for i in range(len(s1)):
+            
+            if i%2:
+                
+                o1[s1[i]]+=1
+                
+                o2[s2[i]]+=1
+                
+            else:
+                
+                e1[s1[i]]+=1
+                
+                e2[s2[i]]+=1
+                
+        return e1==e2 and o1==o2
+            
+            
